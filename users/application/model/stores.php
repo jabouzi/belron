4f7e9bf37ff499e3a2aefce8c16624ac9ac7c4a8
Skander Jabouzi <jabouzi@gmail.com>
2011-04-13 12:36:41 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/belron/trunk@10 e38f5eb3-8ecf-4f33-9ecf-e1f0ffeaab64

---
+++ users/application/model/stores.php
@@ -0,0 +1,45 @@
+<?php
+
+class stores_model
+{
+    public function count()
+    {
+        $table = db('access_stores');       
+        return $table->count()->execute();
+    }
+    
+    public function get_all_distinct()
+    {
+        $table = db('access_stores');
+        $data = $table->select('*')->execute();
+        return $data;
+    }
+    
+    public function get_all_active($province)
+    {
+        $table = db('access_stores');
+        $data = $table->select('*')->where('cart_active','=','1')->clause('AND')->where('province','=',$province)->execute();
+        return $data;
+    }   
+    
+    public function get_store_infos($id)
+    {
+        $table = db('access_stores');
+        $data = $table->select('*')->where('id','=',$id)->execute();
+        return $data;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
